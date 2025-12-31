ad6cfc9f983974ba5ec47793c268bcd92bc02fb6
kingofdead6 <ny_kahlouche@esi.dz>
2025-12-31 22:45:20 +0100

    Added new features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MenuPage from "./components/Menu/MenuPage";
 import Login from "./components/Auth/Login";
-import AdminDashboard from "./Components/Admin/AdminDashboard";
-import AdminCategories from "./Components/Admin/AdminCategories";
-import AdminUsers from "./Components/Admin/AdminUsers";
+import ProtectedRoute from "./components/Shared/ProtectedRoute";
+import AdminDashboard from "./components/Admin/AdminDashboard";
+import AdminCategories from "./components/Admin/AdminCategories";
+import AdminUsers from "./components/Admin/AdminUsers";
 import AdminWorkingHours from "./components/Admin/AdminWorkingHours";
 import AdminMenu from "./components/Admin/AdminMenu";
 import AdminSocialMedia from "./components/Admin/AdminSocialMedia";
@@ -16,6 +17,8 @@ function App() {
       <Routes>
           <Route path="/" element={<MenuPage />} />
           <Route path="/login" element={<Login /> }/>
+
+           <Route element={<ProtectedRoute />}>
               <Route path="/admin/dashboard" element={<AdminDashboard />} />
               <Route path="/admin/categories" element={<AdminCategories />} />
               <Route path="/admin/users" element={<AdminUsers />} />
@@ -23,8 +26,10 @@ function App() {
               <Route path="/admin/menu" element={<AdminMenu />} />
               <Route path="/admin/social-media" element={<AdminSocialMedia />} />
               <Route path="/admin/categories" element={<AdminCategories />} />
+            </Route>
 
             <Route path="*" element={<NotFound />} />
+
       </Routes>
     </Router>
   );
